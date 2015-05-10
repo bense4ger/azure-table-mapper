a385baa8dc734c3d9d7491529a177a01a5e5a529
Ben Seager <github@benseager.co.uk>
2015-05-10 17:42:18 +0100

    Completed table-helper.convertToTableEntity

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # azure-table-mapper
 [![Build Status](https://travis-ci.org/bense4ger/azure-table-mapper.svg)](https://travis-ci.org/bense4ger/azure-table-mapper) [![Coverage Status](https://coveralls.io/repos/bense4ger/azure-table-mapper/badge.svg)](https://coveralls.io/r/bense4ger/azure-table-mapper) [![Dependency Status](https://david-dm.org/bense4ger/azure-table-mapper.svg)](https://david-dm.org/bense4ger/azure-table-mapper) [![devDependency Status](https://david-dm.org/bense4ger/azure-table-mapper/dev-status.svg)](https://david-dm.org/bense4ger/azure-table-mapper#info=devDependencies)
+
 A simple helper which maps objects so they can be stored in Azure Table Storage
+
--- spec/table-helper.spec.js
+++ spec/table-helper.spec.js
@@ -10,6 +10,8 @@ describe('helper.convertToTableEntity', function(){
             id : 1234,
             name : 'test',
             date : new Date(2015, 1, 1),
+            bool : true,
+            object: {value : 'someValue'},
             entityMapping: {
                 partitionKey : 'id',
                 rowKey : {
@@ -19,7 +21,7 @@ describe('helper.convertToTableEntity', function(){
             }
         };
 
-        _tableEntity = helper.convertToTableEntity(_entity, false);
+        _tableEntity = helper.convertToTableEntity(_entity);
     });
     it('should correctly map the partition key value', function(){
         expect(_tableEntity.PartitionKey._).toEqual(_entity.id);
@@ -29,9 +31,17 @@ describe('helper.convertToTableEntity', function(){
     });
     it('should correctly map the object keys', function(){
         expect(_tableEntity.date._).toEqual(_entity.date);
+        expect(_tableEntity.bool._).toEqual(_entity.bool);
+        expect(_tableEntity.object._).toEqual(_entity.object);
     });
-    it('should correctly map the object key types', function(){
+    it('should correctly map the object keys that are dates', function(){
         expect(_tableEntity.date.$).toEqual('Edm.DateTime');
+    });
+    it('should correctly map the object keys that are bools', function(){
+        expect(_tableEntity.bool.$).toEqual('Edm.Boolean');
+    });
+    it('should correctly default mapping to string', function(){
+        expect(_tableEntity.object.$).toEqual('Edm.String');
     });
 });
 
@@ -58,6 +68,58 @@ describe('helper.convertToTableEntity - preserving keys' , function(){
     });
     it('should keep the row key property on the table enttiy', function(){
         expect(_tableEntity.name._).toEqual('test');
+    });
+});
+
+describe('helper.convertToTableEntity - default preserving keys', function(){
+    var _entity, _tableEntity;
+    beforeAll(function(){
+        _entity = {
+            id : 1234,
+            name : 'test',
+            date : new Date(2015, 1, 1),
+            entityMapping: {
+                partitionKey : 'id',
+                rowKey : {
+                    identifier : ['name'],
+                    format : '{name}_rk'
+                }
+            }
+        };
+
+        _tableEntity = helper.convertToTableEntity(_entity);
+    });
+    it('should keep the partiton key property on the table entity', function(){
+        expect(_tableEntity.id._).toEqual(1234);
+    });
+    it('should keep the row key property on the table entity', function(){
+        expect(_tableEntity.name._).toEqual('test');
+    });
+});
+
+describe('helper.convertToTableEntity - do not preserve keys', function(){
+    var _entity, _tableEntity;
+    beforeAll(function(){
+        _entity = {
+            id : 1234,
+            name : 'test',
+            date : new Date(2015, 1, 1),
+            entityMapping: {
+                partitionKey : 'id',
+                rowKey : {
+                    identifier : ['name'],
+                    format : '{name}_rk'
+                }
+            }
+        };
+
+        _tableEntity = helper.convertToTableEntity(_entity, false);
+    });
+    it('should not preserve the original partition key property', function(){
+        expect(_tableEntity[_entity.entityMapping.partitionKey]).toBeUndefined();
+    });
+    it('should not preserve the original row key property(ies)', function(){
+        expect(_tableEntity[_entity.entityMapping.rowKey.identifier[0]]).toBeUndefined();
     });
 });
 
