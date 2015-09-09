2cc9985f88c4b19bcb2aaaaa76666230cc20da10
Ben Seager <github@benseager.co.uk>
2015-09-09 22:11:21 +0100

    Skeleton the spec for new formatter object's functions and added constants.js (not sure if this will be used, but thinking it might be)

---
+++ spec/formatter.spec.js
@@ -0,0 +1,37 @@
+/**
+ * Created by benseager on 09/09/15.
+ */
+var formatter = require('../lib/formatter');
+
+describe('formatter.azurifyRowKey', function () {
+    describe('transforming values to row keys', function () {
+        it('should handle a single value', function () {
+
+        });
+        it('should handle multiple values', function () {
+
+        });
+    });
+    describe('errors are thrown if', function () {
+        it('no value is passed', function () {
+
+        });
+        it('the wrong type of value is passed', function () {
+
+        });
+    });
+});
+
+describe('formatter.parseRowKey', function () {
+    describe('transforming row keys to standard values', function () {
+        it('should handle a single value', function () {
+            
+        });
+        it('should handle multiple values', function () {
+            
+        });
+    });
+    describe('it can fail', function () {
+        
+    });
+});
